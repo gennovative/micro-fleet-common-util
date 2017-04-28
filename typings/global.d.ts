bbc061a88b726d14418a1ae62a4c3cdbc1a33a5e
Rein Chow <rein.chow@firstidea.vn>
2017-04-28 14:43:03 +0700

    Added IAdapter to global definition.

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -14,3 +14,20 @@ interface JsonArray extends Array<string | number | boolean | Date | Json | Json
 // Based on ES6 native Promise definition
 declare type PromiseResolveFn = (value?: any | PromiseLike<any>) => void;
 declare type PromiseRejectFn = (reason?: any) => void;
+
+/**
+ * If an object wants to be initialized when microservice proccess starts, it must
+ * implements this interface to be able to add to adapter list.
+ */
+declare interface IAdapter {
+	/**
+	 * Initializes this adapter.
+	 * @returns A promise that resolves `true` if success, rejects if otherwise.
+	 */
+	init(): Promise<void>;
+	
+	/**
+	 * Stops this adapter and cleans all resources.
+	 */
+	dispose(): Promise<void>;
+}
